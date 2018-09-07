4eb307ff4cc81c17b71d055a7e5c820d12766d05
Gabriela Suriano <surianogabriela@gmail.com>
2018-09-07 13:47:17 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def my_all?(collection)
   i=0 
-  block_return_values = [ ]
+  block_return_values = [true, false, true]
   while i < collection.length 
     block_return_values << yield(collection[i])
     i += 1 
